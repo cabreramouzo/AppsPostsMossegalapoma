f30ea097a0e31f1b04af7422cbefe105189a7741
MAC <miguelangelcabrera1@hotmail.com>
2020-07-29 13:56:52 +0200

    Remove spaces before * in markdown lists to correct a Ink bug of nested unordered lists

--- documentPicker/DocumentParser.swift
+++ documentPicker/DocumentParser.swift
@@ -12,16 +12,22 @@ import SwiftSoup
 import Ink
 
 func parseMarkdown(inputString: String) -> String {
-
+    
+    //remove '  *' because produces a bug into de parser nesting lists to first item
+    let inputStringWithoutSpacesInListItems = inputString.replacingOccurrences(of: "  *", with: "*")
+    
     var mdparser = MarkdownParser()
     let modifier = Modifier(target: .paragraphs, closure: {html,markdown in
         var new_html = html
         var new_html2 = new_html.replacingOccurrences(of: "<p>", with: "")
         return new_html2.replacingOccurrences(of: "</p>", with: "")
     })
+    
     mdparser.addModifier(modifier)
     
-    let html = mdparser.html(from: inputString)
+    
+    
+    let html = mdparser.html(from: inputStringWithoutSpacesInListItems)
     return html
     
 }
