2e9a7260a0d7ba3680ea6c8a430c588b78b6bfbc
MAC <miguelangelcabrera1@hotmail.com>
2020-07-31 13:13:04 +0200

    Add support for upload image to Mac app

--- MacMosseguiPress/ContentView.swift
+++ MacMosseguiPress/ContentView.swift
@@ -12,20 +12,79 @@ struct ContentView: View {
     @State var postTitle: String
     @State var mlpAudioURL: String
     @State var documentUrl: String
+    @State var imagePath: String
+    @State var image: NSImage?
+    @State private var userPickedImage: Bool? = false
+    @State var mediaIDstate: Int
+    var mediaID: Int = -1
+    
+    @State var alert = false
+    @State var alertText = ""
+    @State var alertTitle = ""
     
     @State private var selectorIndex = 0
     
     
-    func pickDocument() {
+    func pickFile() {
         let panel = NSOpenPanel()
-        panel.nameFieldLabel = "Selecciona el document Markdown:"
+        panel.prompt = "Seleccionar"
+        panel.worksWhenModal = true
         panel.nameFieldStringValue = "hola.jpg"
         panel.canCreateDirectories = false
+        panel.canChooseFiles = true
+        panel.allowedFileTypes = ["txt", "md"]
         panel.begin { response in
             if response == NSApplication.ModalResponse.OK, let fileUrl = panel.url {
                 print(fileUrl)
                 self.documentUrl = fileUrl.path
             }
+        }
+        
+    }
+    
+    func pickImage() {
+        let panel = NSOpenPanel()
+        panel.isFloatingPanel = false
+        panel.nameFieldLabel = "Selecciona la imatge principal:"
+        panel.nameFieldStringValue = "hola.jpg"
+        panel.canCreateDirectories = false
+        panel.canChooseFiles = true
+        panel.allowedFileTypes = ["png", "jpeg", "jpg", "gif"]
+        panel.begin { response in
+            if response == NSApplication.ModalResponse.OK, let fileUrl = panel.url {
+                print(fileUrl)
+                self.imagePath = fileUrl.path
+                self.image = NSImage(byReferencingFile: fileUrl.path)
+                self.userPickedImage = true
+
+            }
+            else {
+                self.userPickedImage = false
+            }
+        }
+    }
+    
+    func uploadImage() {
+        self.image = NSImage(byReferencingFile: self.imagePath)
+        if self.image != nil && self.userPickedImage! {
+            print("image funca")
+            uploadImageMac(image: self.image!, imageTitle: "hola_como_estas", fileName: "hola.png", completion: {
+                (is_ok, mediaID) -> Void in
+                self.alert = true
+                print("es ok")
+                print(is_ok)
+                print("mediaID")
+                print(mediaID)
+                self.mediaIDstate = mediaID
+                if is_ok {
+                    self.alertText = "Imatge carregada correctament!"
+                    self.alertTitle = "✅ OK"
+                }
+                else {
+                    self.alertText = "Hi ha hagut un error al pujar l'imatge a Wordpress"
+                    self.alertTitle = "⚠️ Error!"
+                }
+            })
         }
     }
     
@@ -39,22 +98,21 @@ struct ContentView: View {
             Section(header:Text("Arxiu Markdown (Guió)") ) {
                 HStack {
                     Button("seleccionar arxiu", action:{
-                        self.pickDocument()
+                        self.pickFile()
                     })
                     Text(documentUrl)
                     
                 }
             }
             Divider()
-            Section(header:Text("Imatge proncipal") ) {
+            Section(header:Text("Imatge principal") ) {
                 HStack {
                     Button("seleccionar imatge", action:{
-                        print("hola")
-                        let a = "hola"
+                        self.pickImage()
                     })
-                    Text("Ruta: /Users/macma/documents/...")
-                    Button("Pujar imatge", action:{
-                        print("pujar imatge")
+                    Text(imagePath)
+                    Button("pujar imatge", action:{
+                        self.uploadImage()
                     })
                     
                 }
@@ -99,6 +157,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(postTitle: "Programa 42", mlpAudioURL: "https://...", documentUrl: "")
+        ContentView(postTitle: "Programa 42", mlpAudioURL: "https://...", documentUrl: "", imagePath: "", mediaIDstate: -1)
     }
 }
---
+++ MacMosseguiPress/UploadFeaturedImageMac.swift
@@ -0,0 +1,12 @@
+//
+//  UploadFeaturedImageMac.swift
+//  MacMosseguiPress
+//
+//  Created by MAC on 31/07/2020.
+//  Copyright © 2020 MAC. All rights reserved.
+//
+
+import Foundation
+
+
+
