9037b648b5ea854a313c28d9d50afa02ec4a5632
MAC <miguelangelcabrera1@hotmail.com>
2020-09-16 12:25:09 +0200

    Add tests for parse markdown functions

--- MacMosseguiPressTests/MacMosseguiPressTests.swift
+++ MacMosseguiPressTests/MacMosseguiPressTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import MacMosseguiPress
 
 class MacMosseguiPressTests: XCTestCase {
 
@@ -18,10 +19,58 @@ class MacMosseguiPressTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testBasicListElement() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let md = "* hola como estas"
+        let expected = "<ul><li>hola como estas</li></ul>"
+        let html = MacMosseguiPress.parseMarkdown(inputString: md)
+
+        XCTAssertEqual(expected, html)
     }
+    
+    func test2ListElements() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let md = "* hola como estas \n* yo muy bien"
+        let expected = "<ul><li>hola como estas</li><li>yo muy bien</li></ul>"
+        let html = MacMosseguiPress.parseMarkdown(inputString: md)
+
+        XCTAssertEqual(expected, html)
+    }
+    
+    func test2ListElementsWithLinks() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let md = "* hola como estas \n* yo muy [bien](www.esunlink.com) \n* antes habia un link"
+        let expected = "<ul><li>hola como estas</li><li>yo muy <a href=\"www.esunlink.com\">bien</a></li><li>antes habia un link</li></ul>"
+        let html = MacMosseguiPress.parseMarkdown(inputString: md)
+
+        XCTAssertEqual(expected, html)
+    }
+    
+    func test3ListElementsWithLinks() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let md = "* hola como estas \n* yo muy [bien](www.esunlink.com) \n* antes habia un link \n* Aqui tambien hay un [link](www.mossegalapoma.cat)."
+        let expected = "<ul><li>hola como estas</li><li>yo muy <a href=\"www.esunlink.com\">bien</a></li><li>antes habia un link</li><li>Aqui tambien hay un <a href=\"www.mossegalapoma.cat\">link</a>.</li></ul>"
+        let html = MacMosseguiPress.parseMarkdown(inputString: md)
+
+        XCTAssertEqual(expected, html)
+    }
+    
+    func test3ListElementsWithLinksAndSpaces() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let md = "   * hola como estas \n   * yo muy [bien](www.esunlink.com) \n   * antes habia un link \n   * Aqui tambien hay un [link](www.mossegalapoma.cat)."
+        let expected = "<ul><li>hola como estas</li><li>yo muy <a href=\"www.esunlink.com\">bien</a></li><li>antes habia un link</li><li>Aqui tambien hay un <a href=\"www.mossegalapoma.cat\">link</a>.</li></ul>"
+        
+        let html = MacMosseguiPress.parseMarkdown(inputString: md)
+
+        XCTAssertEqual(expected, html)
+    }
+    
+    
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
